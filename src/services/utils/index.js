1b895d5fcf0e62b25acf15ac72281dcfba74f0c3
Bobby Ekaro <iambobbymax@gmail.com>
2022-09-19 13:10:58 +0100

    updates

--- src/services/utils/index.js
+++ src/services/utils/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable quotes */
 export const API = {
   url: "https://tef.test/api/",
+  production: "https://api.tef.ng/api",
 };
 
-export const config = {
-  bucketName: process.env.REACT_APP_AWS_BUCKET,
-  albumName: process.env.REACT_APP_AWS_ALBUM,
-  region: process.env.REACT_APP_AWS_REGION,
-  accessKeyId: process.env.REACT_APP_AWS_KEY,
-  secretAccessKey: process.env.REACT_APP_AWS_SECRET,
-};
+// export const config = {
+//   bucketName: process.env.REACT_APP_AWS_BUCKET,
+//   albumName: process.env.REACT_APP_AWS_ALBUM,
+//   region: process.env.REACT_APP_AWS_REGION,
+//   accessKeyId: process.env.REACT_APP_AWS_KEY,
+//   secretAccessKey: process.env.REACT_APP_AWS_SECRET,
+// };
